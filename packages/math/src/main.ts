74cb77ac158cfebe3c673d52c898e07eb572591a
Mohammed Al-Qurafi <mohammedqurafi@gmail.com>
2022-09-03 21:06:18 +0300

    feat(math): add divide function

--- packages/math/src/main.ts
+++ packages/math/src/main.ts
@@ -1,3 +1,11 @@
 export function add(x: number, y: number) {
 	return x + y;
 }
+
+export function sub(x: number, y: number) {
+	return x - y;
+}
+
+export function div(x: number, y: number) {
+	return x / y;
+}
